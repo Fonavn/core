6a1f40a3363a57e4d09af6de2145f909358a0b9f
ngdnghia28 <ngd.nghia28@gmail.com>
2020-10-27 20:13:04 +0700

    initial for auth

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -1,8 +1,68 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {
+  DynamicModule,
+  HttpModule,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  Provider,
+} from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RuckenCoreModule } from '@lib/core';
+import { authenticate } from 'passport';
+import { AUTH_CONTROLLERS } from './controllers';
+import { AUTH_ENTITIES } from './entities';
+import { AUTH_SERVICES } from './services';
 
-@Module({
-  providers: [AuthService],
-  exports: [AuthService],
-})
-export class AuthModule {}
+@Module({})
+export class RuckenAuthModule implements NestModule {
+  static forFeature(options?: { providers: Provider[] }): DynamicModule {
+    const providers = options && options.providers ? options.providers : [];
+    return {
+      module: RuckenAuthModule,
+      imports: [
+        HttpModule,
+        RuckenCoreModule.forFeature(options),
+        TypeOrmModule.forFeature([...AUTH_ENTITIES]),
+      ],
+      providers: [...providers, ...AUTH_SERVICES],
+      exports: [...AUTH_SERVICES],
+    };
+  }
+  static forRoot(options?: { providers: Provider[] }): DynamicModule {
+    const providers = options && options.providers ? options.providers : [];
+    return {
+      module: RuckenAuthModule,
+      imports: [
+        HttpModule,
+        RuckenCoreModule.forFeature(options),
+        TypeOrmModule.forFeature([...AUTH_ENTITIES]),
+      ],
+      controllers: [...AUTH_CONTROLLERS],
+      providers: [...providers, ...AUTH_SERVICES],
+      exports: [...AUTH_SERVICES],
+    };
+  }
+
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        authenticate('signup', { session: false, passReqToCallback: true }),
+      )
+      .forRoutes('api/auth/signup');
+    consumer
+      .apply(
+        authenticate('signin', { session: false, passReqToCallback: true }),
+      )
+      .forRoutes('api/auth/signin');
+    consumer
+      .apply(
+        authenticate('facebook', { session: false, passReqToCallback: true }),
+      )
+      .forRoutes('api/auth/facebook/token');
+    consumer
+      .apply(
+        authenticate('google', { session: false, passReqToCallback: true }),
+      )
+      .forRoutes('api/auth/google-plus/token');
+  }
+}
--- libs/core/src/core.module.ts
+++ libs/core/src/core.module.ts
@@ -1,8 +1,28 @@
-import { Module } from '@nestjs/common';
-import { CoreService } from './core.service';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CORE_CONTROLLERS } from './controllers';
+import { CORE_ENTITIES } from './entities';
+import { CORE_SERVICES } from './services';
 
-@Module({
-  providers: [CoreService],
-  exports: [CoreService],
-})
-export class CoreModule {}
+@Module({})
+export class RuckenCoreModule {
+  static forFeature(options?: { providers: Provider[] }): DynamicModule {
+    const providers = options && options.providers ? options.providers : [];
+    return {
+      module: RuckenCoreModule,
+      imports: [TypeOrmModule.forFeature([...CORE_ENTITIES])],
+      providers: [...providers, ...CORE_SERVICES],
+      exports: [...CORE_SERVICES],
+    };
+  }
+  static forRoot(options?: { providers: Provider[] }): DynamicModule {
+    const providers = options && options.providers ? options.providers : [];
+    return {
+      module: RuckenCoreModule,
+      imports: [TypeOrmModule.forFeature([...CORE_ENTITIES])],
+      controllers: [...CORE_CONTROLLERS],
+      providers: [...providers, ...CORE_SERVICES],
+      exports: [...CORE_SERVICES],
+    };
+  }
+}
